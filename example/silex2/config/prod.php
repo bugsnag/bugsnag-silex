93a9f570bf46a77e81e8736f50e14446012a8e9f
Graham Campbell <graham@bugsnag.com>
2016-08-16 12:12:23 +0100

    Added bugsnag to the example app

--- example/silex2/config/prod.php
+++ example/silex2/config/prod.php
@@ -2,5 +2,7 @@
 
 // configure your app for the production environment
 
-$app['twig.path'] = array(__DIR__.'/../templates');
-$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+$app['twig.path'] = [__DIR__.'/../templates'];
+$app['twig.options'] = ['cache' => __DIR__.'/../var/cache/twig'];
+
+$app['bugsnag.options'] = ['api_key' => 'YOUR-API-KEY-HERE'];
--- example/silex2/src/app.php
+++ example/silex2/src/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Bugsnag\Silex\Silex2ServiceProvider;
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
@@ -7,14 +8,21 @@ use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
+
+$app->register(new Silex2ServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
     return $twig;
+});
+
+$app->error(function (Exception $error) use ($app) {
+    $app['bugsnag']->notifyException($error);
 });
 
 return $app;
