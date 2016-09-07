fb9ec6e6ce2b944de8e8f1edc6161c53b8e9742a
Graham Campbell <graham@alt-three.com>
2016-09-07 15:26:47 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- example/silex1/src/controllers.php
+++ example/silex1/src/controllers.php
@@ -2,17 +2,13 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
+    return $app['twig']->render('index.html', []);
 })
-->bind('homepage')
-;
+->bind('homepage');
 
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
@@ -20,12 +16,12 @@ $app->error(function (\Exception $e, $code) use ($app) {
     }
 
     // 404.html, or 40x.html, or 4xx.html, or error.html
-    $templates = array(
+    $templates = [
         'errors/'.$code.'.html',
         'errors/'.substr($code, 0, 2).'x.html',
         'errors/'.substr($code, 0, 1).'xx.html',
         'errors/default.html',
-    );
+    ];
 
-    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+    return new Response($app['twig']->resolveTemplate($templates)->render(['code' => $code]), $code);
 });
