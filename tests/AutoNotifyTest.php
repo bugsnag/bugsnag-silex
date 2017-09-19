f9acba3ef04289806aa584986b4de5c97d5ef537
Alex Moinet <alex@bugsnag.com>
2017-09-19 14:50:34 +0100

    Added test

---
+++ tests/AutoNotifyTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Bugsnag\Silex\Tests\Request;
+
+
+use Bugsnag\Client;
+use Bugsnag\Report;
+use Silex\Application;
+use Bugsnag\Silex\Silex2ServiceProvider;
+use GrahamCampbell\TestBenchCore\MockeryTrait;
+use PHPUnit_Framework_TestCase as TestCase;
+use Mockery;
+
+class RequestStub {
+    const MIDDLEWARE_HANDLER = "middleware_handler";
+}
+
+class AutoNotifyTest extends TestCase
+{
+    use MockeryTrait;
+
+    public function testAutoNotifyRespondsCorrectly()
+    {
+        # Create mocks
+        $report = Mockery::namedMock(Report::class, RequestStub::class);
+        $client = Mockery::mock(Client::class);
+        $app = Mockery::mock(Application::class);
+
+        # Create test objects
+        $exception = new \Exception("Test");
+
+        $app->shouldReceive('offsetSet')->with(Mockery::any(), Mockery::any())->andReturnUsing(
+            function($key, $value) use ($app, $exception) {
+                if ($key == 'bugsnag.notifier') {
+                    $value($app)($exception);
+                }
+            }
+        );
+        $app->shouldReceive('before');
+        $app->shouldReceive('offsetGet')->andReturnUsing(
+            function($key) use ($client) {
+                if ($key == 'bugsnag') {
+                    return $client;
+                }
+            }
+        );
+        $report->shouldReceive('fromPHPThrowable')
+            ->with('config', $exception, 'middleware_handler', ['name' => 'silex'])
+            ->once()
+            ->andReturn($report);
+        $client->shouldReceive('getConfig')->once()->andReturn('config');
+        $client->shouldReceive('notify')->once()->with($report, Mockery::any());
+
+        # Initiate test
+        $serviceProvider = new Silex2ServiceProvider();
+        $serviceProvider->register($app);
+    }
+    
+}
