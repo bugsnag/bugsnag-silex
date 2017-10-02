bb0067531c14afab68ee566bf9f8df5ed4432f1a
Alex Moinet <alex@bugsnag.com>
2017-10-02 16:18:27 +0100

    Using env vars to seperate tests

--- tests/AutoNotifyTest.php
+++ tests/AutoNotifyTest.php
@@ -20,12 +20,13 @@ class AutoNotifyTest extends TestCase
 {
     use MockeryTrait;
 
-    public function testSilex1AutoNotify()
+    public function testAutoNotify()
     {
-        $phpVersion56 = version_compare(PHP_VERSION, "5.6", ">=");
-        if ($phpVersion56) $this->markTestSkipped("Silex1 only tested in version 5.5.9");
-
-        $Silex1ServiceProvider = "Bugsnag\Silex\Silex1ServiceProvider";
+        if (getenv("MINIMAL_DEPENDENCIES")) {
+            $SilexServiceProvider = "Bugsnag\Silex\Silex1ServiceProvider";
+        } else {
+            $SilexServiceProvider = "Bugsnag\Silex\Silex2ServiceProvider";
+        }
 
         # Create mocks
         $report = Mockery::namedMock(Report::class, RequestStub::class);
@@ -53,57 +54,16 @@ class AutoNotifyTest extends TestCase
             }
         );
         $report->shouldReceive('fromPHPThrowable')
-            ->with('config', $exception, true, ['type' => 'unhandledExceptionMiddleware', 'attributes' => ['framework' => 'Silex']])
+            ->with('config', $exception)
             ->once()
             ->andReturn($report);
+        $report->shouldReceive('setUnhandled')->once()->with(true);
+        $report->shouldReceive('setSeverityReason')->once()->with(['type' => 'unhandledExceptionMiddleware', 'attributes' => ['framework' => 'Silex']]);
         $client->shouldReceive('getConfig')->once()->andReturn('config');
         $client->shouldReceive('notify')->once()->with($report, Mockery::any());
 
         # Initiate test
-        $serviceProvider = new $Silex1ServiceProvider;
-        $serviceProvider->register($app);
-    }
-
-    public function testSilex2AutoNotify()
-    {
-        $phpVersion56 = version_compare(PHP_VERSION, "5.6", ">=");
-        if (!$phpVersion56) $this->markTestSkipped("Silex2 not tested in version 5.5.9");
-
-        $Silex2ServiceProvider = "Bugsnag\Silex\Silex2ServiceProvider";
-
-        # Create mocks
-        $report = Mockery::namedMock(Report::class, RequestStub::class);
-        $client = Mockery::mock(Client::class);
-        $app = Mockery::mock(Application::class);
-
-        # Create test objects
-        $exception = new \Exception("Test");
-
-        $app->shouldReceive('offsetSet')->with(Mockery::any(), Mockery::any())->andReturnUsing(
-            function($key, $value) use ($app, $exception) {
-                if ($key == 'bugsnag.notifier') {
-                    $notifyFunc = call_user_func($value, $app);
-                    call_user_func($notifyFunc, $exception);
-                }
-            }
-        );
-        $app->shouldReceive('before');
-        $app->shouldReceive('offsetGet')->andReturnUsing(
-            function($key) use ($client) {
-                if ($key == 'bugsnag') {
-                    return $client;
-                }
-            }
-        );
-        $report->shouldReceive('fromPHPThrowable')
-            ->with('config', $exception, true, ['type' => 'unhandledExceptionMiddleware', 'attributes' => ['framework' => 'Silex']])
-            ->once()
-            ->andReturn($report);
-        $client->shouldReceive('getConfig')->once()->andReturn('config');
-        $client->shouldReceive('notify')->once()->with($report, Mockery::any());
-
-        # Initiate test
-        $serviceProvider = new $Silex2ServiceProvider;
+        $serviceProvider = new $SilexServiceProvider;
         $serviceProvider->register($app);
     }
     
